6c2bec21eefc647c9a92981fa1ab241896d6cb47
Luca Alcalde <luke707@gmail.com>
2022-08-20 19:06:47 -0400

    Minor fixes

--- src/Pittacusw/Core/Providers/CoreServiceProvider.php
+++ src/Pittacusw/Core/Providers/CoreServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Pittacusw\Core\Providers;
 
-use Illuminate\Routing\Router;
-use Pittacusw\Core\Support\Package;
+use Illuminate\Contracts\Http\Kernel;
+use Spatie\LaravelPackageTools\Package;
 use Pittacusw\Core\Commands\GitAddCommand;
 use Pittacusw\Core\Commands\GitPullCommand;
 use Pittacusw\Core\Middlewares\SecurityHeaders;
@@ -12,12 +12,16 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class CoreServiceProvider extends PackageServiceProvider {
 
+  public function bootingPackage() {
+    $kernel = app(Kernel::class);
+    $kernel->pushMiddleware(SecurityHeaders::class);
+  }
+
   public function configurePackage(Package $package)
   : void {
     $package->name('pittacusw-core')
             ->hasConfigFile()
             ->hasMigration('create_github_webhook_calls_table')
-            ->hasMiddleware(SecurityHeaders::class)
             ->hasCommand(GitAddCommand::class)
             ->hasCommand(GitPullCommand::class)
             ->hasCommand(ComposerInstallCommand::class);
